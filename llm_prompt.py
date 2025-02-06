84e6a0687b335dbad21101817054bb1b46a9124f
Yash <yashaoute2812@gmail.com>
2025-02-06 20:04:16 +0530

    pdf update

--- llm_prompt.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.prompts import ChatPromptTemplate
-
-def get_chat_prompt():
-    prompt = ChatPromptTemplate([
-    ("system","your personalized assitant. Please respond to user requests"),
-    ("user","Questions:{question}")])
-    return prompt
-
-
